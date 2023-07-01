f90b848f87ff0a3643048302242d4f7f92f688d3
edwin martinez <alpha0202@gmail.com>
2023-07-01 11:21:22 -0500

    Implementación de los métodos del repositorio.

--- Blazor.Contacts.Wasm.Repository/ContactRepository.cs
+++ Blazor.Contacts.Wasm.Repository/ContactRepository.cs
@@ -1,4 +1,5 @@
 ﻿using Blazor.Contacts.Wasm.Shared;
+using Dapper;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -17,29 +18,99 @@ namespace Blazor.Contacts.Wasm.Repository
                 _dbConnection = dbConnection;
         }
 
-        public Task<IEnumerable<Contact>> GetAll()
+        public async Task<IEnumerable<Contact>> GetAll()
         {
-            throw new NotImplementedException();
-        }
-        public Task<Contact> GetDetails(int id)
-        {
-            throw new NotImplementedException();
-        }
-        public Task<bool> InsertContact(Contact contact)
-        {
-            throw new NotImplementedException();
-        }
+            var sql = @"SELECT Id, FirstName, LastName, Phone, Address 
+                        FROM ContactsDB
+                        ";
 
-        public Task<bool> DeleteContact(int id)
-        {
-            throw new NotImplementedException();
+            return await _dbConnection.QueryAsync<Contact>(sql, new {  });
         }
 
 
 
-        public Task<bool> UpdateContact(Contact contact)
+        public async Task<Contact> GetDetails(int id)
         {
-            throw new NotImplementedException();
+            var sql = @"SELECT Id, FirstName, LastName, Phone, Address 
+                        FROM ContactsDB
+                        WHERE Id = @Id"; 
+
+            return await _dbConnection.QueryFirstOrDefaultAsync<Contact>(sql, new {Id = id});
         }
+
+
+
+        public async Task<bool> InsertContact(Contact contact)
+        {
+            try
+            {
+                var sql = @"INSERT INTO ContactsDB(FirstName, LastName, Phone, Address)
+                                    VALUES(@FirstName, @LastName, @Phone, @Address)";
+
+
+                var result = await _dbConnection.ExecuteAsync(sql, 
+                    new
+                    {
+                         contact.FirstName, 
+                         contact.LastName,
+                         contact.Phone,
+                         contact.Adrress
+                    }                
+                    );
+
+                return result > 0;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+
+
+
+        public async Task<bool> UpdateContact(Contact contact)
+        {
+            try
+            {
+                var sql = @"UPDATE ContactsDB
+                                    SET FirstName = @FirstName  , 
+                                    LastName = @LastName , 
+                                    Phone = @Phone , 
+                                    Address = @Address 
+                                    WHERE Id = @Id";
+
+
+                var result = await _dbConnection.ExecuteAsync(sql,
+                    new
+                    {
+                        contact.Id,
+                        contact.FirstName,
+                        contact.LastName,
+                        contact.Phone,
+                        contact.Adrress
+                    }
+                    );
+
+                return result > 0;
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+
+        public  async Task<bool> DeleteContact(int id)
+        {
+            var sql = @"DELETE FROM ContactsDB WHERE Id = @Id";
+
+            var result = await _dbConnection.ExecuteAsync(sql, new { Id = id });
+        }
+
     }
 }
