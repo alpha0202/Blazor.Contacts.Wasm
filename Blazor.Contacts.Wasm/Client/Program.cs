2da062fd81959b5e70291fb07fc48d6bde0dbe6e
edwin martinez <edwmartinezg@outlook.com>
2023-07-09 12:46:24 -0500

    Trabajando con los componentes Radzen.

--- Blazor.Contacts.Wasm/Client/Program.cs
+++ Blazor.Contacts.Wasm/Client/Program.cs
@@ -2,11 +2,21 @@ using Blazor.Contacts.Wasm.Client;
 using Blazor.Contacts.Wasm.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+
+//Radzen componentes
+builder.Services.AddScoped<DialogService>();
+builder.Services.AddScoped<NotificationService>();
+builder.Services.AddScoped<TooltipService>();
+builder.Services.AddScoped<ContextMenuService>();
+
+
+//interfaz del servicio.
 builder.Services.AddScoped<IContactService, ContactService>();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
